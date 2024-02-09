47a6e5e60f6676b35f14934f14aa9872052c8b2c
Delioos <htz_j@protonmail.com>
2024-02-09 11:56:14 +0100

    feat(auth): sign in logic setup

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = () => {
+    const {isAuthenticated, isLoading} = useConvexAuth;
+
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -13,10 +19,25 @@ export const Heading = () => {
                 Jotion is the connected workspace where <br />
                 better, fast work happens.
             </h3>
+            {isLoading &&(
+                <div className="w-full flex items-center justify-center">
+                <Spinner size="lg" />
+                </div>
+            )}
+            {isAuthenticated && !isLoading (
             <Button>
+                <Link href="/documents">
                 Enter Jotion
                 <ArrowRight className="h-4 w-4 ml-2" />
+                </Link>
             </Button>
+            )}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton mode="modal">
+                    Get Jotion Free
+                    <ArrowRight className="h-4"  />
+                </SignInButton>
+            )}
         </div>
     )
 }
